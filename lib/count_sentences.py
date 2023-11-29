9fff69fdc4da33b98e91aa9a119f2d0a3b64c657
Kevin <kevinolongo9@gmail.com>
2023-11-29 13:04:51 +0300

    Complete lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -2,3 +2,27 @@
 
 class MyString:
   pass
+  def __init__(self, value=""):
+        if type(value) != str:
+            print("The value must be a string.")
+            raise ValueError("The value must be a string.")
+        self.value = value
+
+  def is_sentence(self):
+        return self.value.endswith(".")
+
+  def is_question(self):
+        return self.value.endswith("?")
+
+  def is_exclamation(self):
+        return self.value.endswith("!")
+
+  def count_sentences(self):
+        sentence_endings = ['.', '?', '!']
+        return sum(self.value.endswith(ending) for ending in sentence_endings)
+
+
+# Example usage:
+# string = MyString()
+# string.value = "This is a string! It has three sentences. Right?"
+# print(string.count_sentences())  # Output: 3
